bb8458daee9360b9bda5c88789fa33a4092c78c7
heikneuter <jorritdesch@gmail.com>
2020-05-14 12:19:10 +0200

    Made a rough version of the User Interface (Main UI not the individual Interfaces for the buttons).

---
+++ Monster Bar/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+    public Canvas canvas;
+    
+    public void InstantiatePanel(GameObject panel)
+    {
+        Instantiate(panel, new Vector3(0, 0, 0), Quaternion.identity, canvas.transform);
+    }
+
+    public void RemovePanel(GameObject panel)
+    {
+        Destroy(panel);
+    }
+
+}
