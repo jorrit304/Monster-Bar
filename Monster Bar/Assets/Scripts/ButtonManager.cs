ea9b813c01117516a1ddc3a7a30630b6c96bba55
Jorrit Deschaux <jorritdesch@gmail.com>
2020-05-18 11:39:32 +0200

    Made sure that only one panel is allowed in the game at a time. Now working on a close button for the UI panel.

--- Monster Bar/Assets/Scripts/ButtonManager.cs
+++ Monster Bar/Assets/Scripts/ButtonManager.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class ButtonManager : MonoBehaviour
 {
+
     public Canvas canvas;
+
+    public GameObject PanelPrefab;
+    private GameObject Panel;
     
-    public void InstantiatePanel(GameObject panel)
+    public void InstantiatePanel()
     {
-        Instantiate(panel, new Vector3(0, 0, 0), Quaternion.identity, canvas.transform);
+        if(Panel == null)
+        {
+            Panel = Instantiate(PanelPrefab, new Vector3(0, 0, 0), Quaternion.identity, canvas.transform) as GameObject;
+        }
+
     }
 
-    public void RemovePanel(GameObject panel)
+    public void RemovePanel()
     {
-        Destroy(panel);
+        Destroy(Panel);
     }
 
 }
